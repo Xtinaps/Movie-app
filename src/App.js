391249afe12bb64b09e16aca7d919be8d7773631
Xtinaps <xtinasparakal@gmail.com>
2024-07-20 12:53:12 +0530

    viewall created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddMovie from './components/AddMovie';
+import ViewAllMovie from './ViewAllMovie';
 
 function App() {
   return (
     <div>
       <AddMovie/>
+      <ViewAllMovie/>
     </div>
   );
 }
