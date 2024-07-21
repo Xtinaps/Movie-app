7ae15388ed94a7e6d830d8ae9e15870463f76de3
Xtinaps <xtinasparakal@gmail.com>
2024-07-21 21:40:13 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import AddMovie from './components/AddMovie';
-import ViewAllMovie from './ViewAllMovie';
+import ViewAllMovie from './components/ViewAllMovie';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddMovie/>
-      <ViewAllMovie/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+
+     <Route path='/' element={<AddMovie/>}/>
+     <Route path='/View' element={<ViewAllMovie/>}/>
+
+    </Routes>
+    
+    </BrowserRouter>
   );
 }
 
